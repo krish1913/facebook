9f803f68335fa5e1791125804fafa0d3c5959478
krish1913 <muralikrishna00467@gmail.com>
2018-07-22 12:01:39 -0700

    about apple

---
+++ apple.java
@@ -0,0 +1,10 @@
+about apple
+" https://en.wikipedia.org/wiki/Apple_Inc. "
+
+Bibliography
+
+  Rob Price (1987). So Far: The First Ten Years of a Vision. Apple Computer. ISBN 978-1-55693-974-7.
+  Ken Polsson. "Chronology of Events in the History of Microcomputers". Archived from the original on June 2, 2008. Retrieved August 18, 2008.
+    "Apple II history". Retrieved August 18, 2008.
+    "Apple III history". Retrieved August 5, 2006.
+    "Apple's 2012 Annual Report: More Employees, More Office Space, More Sales".
